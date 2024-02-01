f9e4decc1dc1cddfbe125b9440e2fdbd5b532e00
santiago arteche <santiarteche@hotmail.com>
2024-02-01 04:11:36 -0300

    flatList, addItem and deleteItem added

--- App.js
+++ App.js
@@ -1,10 +1,70 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Text } from "react-native";
+import { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  FlatList,
+  Pressable,
+} from "react-native";
 
+const productsArray = [
+  { name: "Zapatillas", id: 1 },
+  { name: "Remera", id: 2 },
+  { name: "Campera", id: 3 },
+  { name: "Bolso", id: 4 },
+];
 export default function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [itemList, setItemList] = useState(productsArray);
+
+  const handleInputValue = (value) => setInputValue(value);
+
+  const deleteItem = (id) => {
+    const prodsFiltrados = itemList.filter((prods) => prods.id !== id);
+    setItemList(prodsFiltrados);
+  };
+
+  const date = Date.now();
+
+  const addItemToList = () => {
+    const object = {
+      name: inputValue,
+      id: date,
+    };
+
+    setItemList([...itemList, object]);
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.welcomeCoder}>Hola, Coder!</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholderTextColor={"gray"}
+          placeholder="Ingrese un producto..."
+          value={inputValue}
+          onChangeText={handleInputValue}
+        />
+        <Pressable onPress={addItemToList}>
+          <Text style={styles.inputPressable}>Anadir a la lista</Text>
+        </Pressable>
+      </View>
+      <View>
+        <FlatList
+          data={itemList}
+          renderItem={({ item }) => (
+            <View style={{ display: "flex", flexDirection: "row", gap: 10 }}>
+              <Text>{item.name}</Text>
+              <Pressable onPress={() => deleteItem(item.id)}>
+                <Text>Borrar</Text>
+              </Pressable>
+            </View>
+          )}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +75,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
   },
   welcomeCoder: {
     backgroundColor: "red",
@@ -25,5 +84,29 @@ const styles = StyleSheet.create({
     color: "black",
     fontWeight: "bold",
     fontSize: 50,
+  },
+  input: {
+    fontSize: 20,
+    marginVertical: 20,
+    paddingVertical: 30,
+    paddingHorizontal: 10,
+    height: 40,
+    width: 500,
+    borderColor: "black",
+    borderWidth: 3,
+  },
+  inputContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 10,
+  },
+  inputPressable: {
+    backgroundColor: "black",
+    padding: 10,
+    fontSize: 20,
+    borderRadius: 5,
+    color: "white",
   },
 });
